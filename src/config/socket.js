928baaf200009aebdf9cb569e0966c10ae3b996e
roufurrohim <muhamadbarruroufurrohim@gmail.com>
2021-11-26 22:54:46 +0700

    final for deploy

--- src/config/socket.js
+++ src/config/socket.js
@@ -1,7 +1,10 @@
 import { io } from "socket.io-client";
-import { SOCKET_URL } from "../helpers/env";
+// import { Manager } from "socket.io-client";
+import { API_URL } from "../helpers/env";
 
-const socket = io(`${SOCKET_URL}`);
+// const manager = new Manager(`${SOCKET_URL}`);
+// const socket = manager.socket("/");
+const socket = io(`${API_URL}`);
 // const socket = io(SOCKET_URL)
 // const socket = io("http://localhost:5000");
 
